cff5547885b6e2829773ca76c980dc65ef1e4e6d
Tolibjon0601 <egamberdiyevtolibjon0601@gmail.com>
2024-05-22 19:32:23 +0500

    start

---
+++ findEvenNumbers.js
@@ -0,0 +1,7 @@
+export const EvenNumber =(num)=>{
+ if(num%2==0){
+  return "juft son"
+ }else{
+  return "toq son"
+ }
+}
---
+++ main.js
@@ -0,0 +1,6 @@
+import { EvenNumber } from "./findEvenNumber.js";
+let variants = Number(prompt("1. juft son   2.toq son"));
+if (variants)==1 {
+let num=Number(prompt("sonni kiriting"));
+alert(EvenNumber(num));
+}
